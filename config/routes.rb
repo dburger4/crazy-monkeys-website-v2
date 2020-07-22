b9aa60b1f5823c5b35f0df589eb6e80622ff1a84
Daniel Burger <burger5@purdue.edu>
2020-07-22 00:13:27 -0400

    new progress on styling, navbar

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   resources :forms
   root 'welcome#index'
   devise_for :users
-  get '/auditions', to: 'auditions#info'
+  get '/auditions', to: 'auditions#info', as: 'auditions_info'
   get '/auditions/new', to: 'auditions#new', as: 'auditions_new'
 end
